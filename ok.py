6c4069aa8a99772dc3d9536f4d39fd17dcdcb1fa
juqccc <159039704+juqccc@users.noreply.github.com>
2024-02-27 23:39:26 +0000

    First 27

--- ok.py
+++ ok.py
@@ -1,1 +1,10 @@
+import time
 
+
+def timew (message, delay = 0.1):
+    for char in message:
+        print (char, end='', flush = True)
+        time.sleep(delay)
+    print ()
+
+timew ('wassup')
